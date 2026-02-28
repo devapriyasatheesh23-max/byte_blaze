3a39cfc28cb200117f89c0862598c7fa70d0c04a
devapriyasatheesh23-max <devapriyasatheesh23@gmail.com>
2026-02-28 15:41:01 +0530

    intital commit

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjs-KcE1mxGarAwUVtCo1thM-Oyu_6IMQ",
+  authDomain: "digitalwitness-91e06.firebaseapp.com",
+  projectId: "digitalwitness-91e06",
+  storageBucket: "digitalwitness-91e06.firebasestorage.app",
+  messagingSenderId: "594137132842",
+  appId: "1:594137132842:web:42a87acaee87abc17bb853"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
